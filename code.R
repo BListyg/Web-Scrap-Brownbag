41a18c567fa604aa120ceff0accd01cab60e2ffc
Benjamin Listyg <listyg.ben@gmail.com>
2017-10-06 08:53:08 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,2 +1,13 @@
-# webscrapbb
-RIASEC Data
+#Install and load “rvest” package
+
+Install.packages(“rvest”)
+library(rvest)
+
+riasec.scrape = function(x,y) {read_html(x) %>% 
+		     html_nodes(y) %>% 
+         html_text()}
+
+cbind(
+riasec.scrape(x = "https://www.onetonline.org/link/details/11-9013.01", y = "#wrapper_Interests .report2a b"), 
+riasec.scrape(x = "https://www.onetonline.org/link/details/11-9013.01", y = "#wrapper_Interests .moreinfo b")
+)
